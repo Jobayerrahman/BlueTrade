2cffe257845495e5ac4e0ab6ff30cc386cb91240
jobayerrahman <jobayer.aiub@gmail.com>
2023-09-19 18:39:52 +0600

    Charts drop-down create and style correction

--- app/components/Charts/Charts.js
+++ app/components/Charts/Charts.js
@@ -1,10 +1,80 @@
 import React from 'react';
 import styles from './Charts.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-function Charts(props) {
+function Charts() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(!open);
+    };
     return (
         <div className={styles.chartsWrapper}>
-            
+            <div className={styles.chartsHeader}>
+                <div className={styles.chartsDropdown}>
+                    <div onClick={handleOpen} className={styles.chartsDropdownHeader}>
+                        <div className={styles.chartsDropdownCurrency}>
+                            <h2>BTCUSDT</h2>
+                            <span>Bitcoin</span>
+                        </div>
+                        <div className={styles.chartsDropdownAmount}>
+                            <h2>$23,738</h2>
+                            <div className={styles.chartsDropdownPercentage}>
+                                <h4>+23,6%</h4>
+                            </div>
+                        </div>
+                        <FontAwesomeIcon className={styles.menuIcon} icon={faCaretDown} />
+                    </div>
+                    {open ? (
+                        <div className={styles.chartsDropdownBody}>
+                            <div className={styles.chartsDropdownItem}>
+                                <div className={styles.chartsDropdownCurrency}>
+                                    <h2>BTCUSDT</h2>
+                                    <span>Bitcoin</span>
+                                </div>
+                                <div className={styles.chartsDropdownAmount}>
+                                    <h2>$23,738</h2>
+                                    <div className={styles.chartsDropdownPercentage}>
+                                        <h4>+23,6%</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.chartsDropdownItem}>
+                                <div className={styles.chartsDropdownCurrency}>
+                                    <h2>BTCUSDT</h2>
+                                    <span>Bitcoin</span>
+                                </div>
+                                <div className={styles.chartsDropdownAmount}>
+                                    <h2>$23,738</h2>
+                                    <div className={styles.chartsDropdownPercentage}>
+                                        <h4>+23,6%</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.chartsDropdownItem}>
+                                <div className={styles.chartsDropdownCurrency}>
+                                    <h2>BTCUSDT</h2>
+                                    <span>Bitcoin</span>
+                                </div>
+                                <div className={styles.chartsDropdownAmount}>
+                                    <h2>$23,738</h2>
+                                    <div className={styles.chartsDropdownPercentage}>
+                                        <h4>+23,6%</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : null}
+                </div>
+                <div className={styles.chartsMenu}>
+                    <button className={styles.active}>1hr</button>
+                    <button>2hr</button>
+                    <button>3hr</button>
+                    <button>1W</button>
+                    <button>1M</button>
+                </div>
+            </div>
         </div>
     );
 }
--- app/components/Convert/Convert.js
+++ app/components/Convert/Convert.js
@@ -6,6 +6,16 @@ import { useState } from 'react';
 
 function Convert() {
     const [currency, setCurrency] = useState('');
+    const [openUSDT, setOpenUSDT] = React.useState(false);
+    const [openBTC, setOpenBTC] = React.useState(false);
+
+    const handleUSDTOpen = () => {
+        setOpenUSDT(!openUSDT);
+    };
+
+    const handleBTCOpen = () => {
+        setOpenBTC(!openBTC);
+    };
 
     const handleInput = (e) =>{
         if(e.target.type ==='text'){
@@ -24,17 +34,35 @@ function Convert() {
             </div>
             <div className={styles.convertCurrencyInputWrapper}>
                 <input className={styles.currencyInput} type="text" value={currency} onChange={handleInput}/>
-                <button className={styles.currencyButton} style={{ backgroundColor: '#C7E4DB', color:'#26AB5F' }}>
-                    USDT 
-                    <FontAwesomeIcon className={styles.menuIcon} icon={faCaretDown} />
-                </button>
+                <div className={styles.currencyDropdownUSDT}>
+                    <div onClick={handleUSDTOpen} className={styles.currencyDropdownHeader}>
+                        USDT 
+                        <FontAwesomeIcon className={styles.menuIcon} icon={faCaretDown} />
+                    </div>
+                    {openUSDT ? (
+                        <div className={styles.currencyDropdownBody}>
+                            <div className={styles.currencyDropdownItem}>Menu 1</div>
+                            <div className={styles.currencyDropdownItem}>Menu 2</div>
+                            <div className={styles.currencyDropdownItem}>Menu 3</div>
+                        </div>
+                    ) : null}
+                </div>
             </div>
             <div className={styles.convertCurrencyInputWrapper}>
                 <input className={styles.currencyInput} type="text" value={currency} onChange={handleInput}/>
-                <button className={styles.currencyButton} style={{ backgroundColor: '#FCF4DB', color:'#FF7C04' }}>
-                    BTC
-                    <FontAwesomeIcon className={styles.menuIcon} icon={faCaretDown} />
-                </button>
+                <div className={styles.currencyDropdownBTC}>
+                    <div onClick={handleBTCOpen} className={styles.currencyDropdownHeader}>
+                        BTC
+                        <FontAwesomeIcon className={styles.menuIcon} icon={faCaretDown} />
+                    </div>
+                    {openBTC ? (
+                        <div className={styles.currencyDropdownBody}>
+                            <div className={styles.currencyDropdownItem}>Menu 1</div>
+                            <div className={styles.currencyDropdownItem}>Menu 2</div>
+                            <div className={styles.currencyDropdownItem}>Menu 3</div>
+                        </div>
+                    ) : null}
+                </div>
             </div>
             <button className={styles.convertButton}>Convert Now</button>
             <p>The ultimate price and output is determined by the amount of tokens in the pool at the time of your swap.</p>
